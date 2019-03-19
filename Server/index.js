1aa69ee4dbb8faccec0fafcc696e3056959a2d11
charles <charles.onuorah@yahoo.com>
2019-03-19 21:52:26 +0100

    register a new user

--- Server/index.js
+++ Server/index.js
@@ -2,6 +2,9 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import mongoose from 'mongoose';
+
+import { User} from './Models/User'
+
 import 'dotenv/config'
 
 const app = express();
@@ -17,6 +20,19 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.use(cookieParser());
+
+app.post('/api/users/register', (req, res) => {
+    const user = new User(req.body);
+    user.save((err, doc) => {
+        if(err) return res.status(400)
+                .send({success:false, err})
+        res.status(200).send({
+            success: true,
+            user: doc
+        })
+    })
+    
+})
 
 app.listen(port, () => {
     console.log(`Server is Listening at ${port}`)
