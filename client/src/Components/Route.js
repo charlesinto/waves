f79e198071e56fdafb3c51106db0dcae3b0914d8
charles <charles.onuorah@yahoo.com>
2019-03-22 23:49:57 +0100

    feat-secure route-12892145721

--- client/src/Components/Route.js
+++ client/src/Components/Route.js
@@ -5,16 +5,18 @@ import Layout from './HOC/Layout';
 import RegisterLogin from './RegisterLogin';
 import Register from './RegisterLogin/Register';
 import UserDashboard from './User';
+import AuthCheck from './HOC/AuthCheck';
+import { PRIVATE, PUBLIC, AUTH_RESTRICTED} from './Actions/types';
 
 class Routes extends Component {
     render() {
         return (
             <Layout>
                 <Switch>
-                    <Route path="/" exact component={Home} />
-                    <Route path="/users/dashboard" exact component={UserDashboard} />
-                    <Route path="/register_login" exact component={RegisterLogin} />
-                    <Route path="/register" exact component={Register} />
+                    <Route path="/" exact component={AuthCheck(Home, PUBLIC)} />
+                    <Route path="/users/dashboard" exact component={AuthCheck(UserDashboard, PRIVATE)} />
+                    <Route path="/register_login" exact component={AuthCheck(RegisterLogin, AUTH_RESTRICTED)} />
+                    <Route path="/register" exact component={AuthCheck(Register, AUTH_RESTRICTED)} />
                 </Switch>
             </Layout>
         );
